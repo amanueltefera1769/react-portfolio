45a1986d9c68f48de81d31ab43bf5ae7dd827137
Amanuel <52597872+amanueltefera1769@users.noreply.github.com>
2023-02-22 13:53:32 +0300

    services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,116 @@
 import React from 'react'
 import './services.css'
+import {BsCheck2} from 'react-icons/bs'
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+
+            <h3> UI/UX Design</h3>
+
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            
+
+          </ul>
+          
+        </article>
+        {/* End of ux/ui */}
+
+        <article className='service'>
+          <div className='service__head'>
+
+            <h3> Web Development</h3>
+
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            
+
+          </ul>
+          
+        </article>
+        {/* End of Web Development */}
+        <article className='service'>
+          <div className='service__head'>
+
+            <h3> Content Creation</h3>
+
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ut consectetur iure sequi pariatur cum saepe alias ipsum, autem maiores ratione modi sed esse voluptatum illum cumque sit rerum voluptate!</p>
+            </li>
+            
+
+          </ul>
+          
+        </article>
+        {/* End of Content Creation */}
+        
+      </div>
+    </section>
   )
 }
 
